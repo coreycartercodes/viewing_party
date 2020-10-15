3955d28970a138c3286c6e9ea320e53ceecd6c14
Logan Riffell <lkriffell@gmail.com>
2020-10-14 18:06:13 -0600

    Update guest_spec

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe Guest, type: :model do
-  describe "validations" do
-    xit {should validate_presence_of(:party)}
-    xit {should validate_presence_of(:user)}
-  end
   describe "relationships" do
     it {should belong_to :user}
     it {should belong_to :party}
